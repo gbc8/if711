6c62a8e298b7ae69fa46ea1b4d7ca008d5ab501c
gbc8 <gbc7@cin.ufpe.br>
2022-08-07 20:59:02 -0300

    Adding Exercicio 2

---
+++ Exercicio 2/1.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+type Sincronizar struct {
+	produto      int
+	consumidores chan chan int
+}
+
+func (d *Sincronizar) Produtor(proChan chan int) {
+	for {
+		d.produto = <-proChan
+		d.Cast()
+	}
+}
+
+func (d *Sincronizar) Cast() {
+	if len(d.consumidores) <= 1 {
+		ch := <-d.consumidores
+		ch <- d.produto
+	} else {
+		for len(d.consumidores) != 0 {
+			ch := <-d.consumidores
+			ch <- d.produto
+		}
+	}
+}
+
+func (d *Sincronizar) consumir(wg *sync.WaitGroup) int {
+	responseChan := make(chan int)
+	d.consumidores <- responseChan
+	response := <-responseChan
+	return response
+}
+
+func main() {
+
+	wg := sync.WaitGroup{}
+
+	sc := &Sincronizar{consumidores: make(chan chan int, 100)}
+
+	seed := rand.NewSource(time.Now().UnixNano())
+	gen := rand.New(seed)
+
+	produtos := make(chan int)
+	go sc.Produtor(produtos)
+
+	reader := bufio.NewReader(os.Stdin)
+INPUT:
+	for {
+		char, _, _ := reader.ReadRune()
+
+		switch char {
+		case 'c':
+			go consumir(&wg, sc)
+		case 'p':
+			i := gen.Intn(100)
+			fmt.Println("PRODUZINDO", i)
+			produtos <- i
+		case 'k':
+			break INPUT
+		}
+	}
+}
+
+func consumir(wg *sync.WaitGroup, dt *Sincronizar) {
+	wg.Add(1)
+	fmt.Println("ADICIONANDO CONSUMIDOR")
+	ch := make(chan int)
+	dt.consumidores <- ch
+	fmt.Println("RECEBEU", <-ch)
+}
---
+++ Exercicio 2/2.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	b := NewBarbearia(3)
+	wg := sync.WaitGroup{}
+
+	go b.Barbeiro(&wg)
+	go b.AbrirBarbearia(&wg)
+
+	for i := 0; i < 10; i++ {
+		c := Cliente{id: i}
+		wg.Add(1)
+		go c.Entrar(b.entrada)
+	}
+	wg.Wait()
+}
+
+type Barbearia struct {
+	cadeiras         int
+	ocupadas         int
+	barbeiroAcordado bool
+	entrada          chan *Cliente
+	cortando         chan *Cliente
+}
+
+func NewBarbearia(capacidade int) *Barbearia {
+	b := Barbearia{
+		cadeiras:         capacidade,
+		ocupadas:         0,
+		barbeiroAcordado: false,
+		entrada:          make(chan *Cliente),
+		cortando:         make(chan *Cliente),
+	}
+
+	return &b
+}
+
+func (b *Barbearia) Barbeiro(wg *sync.WaitGroup) {
+	fmt.Println("Barbeiro chegou para trabalhar")
+	for {
+		if b.ocupadas == 0 {
+			fmt.Println("Barbeiro foi dormir")
+			b.barbeiroAcordado = false
+		}
+		Cliente := <-b.cortando
+		if !b.barbeiroAcordado {
+			fmt.Println("Cliente", Cliente.id, "acordou o barbeiro")
+		}
+		b.barbeiroAcordado = true
+		b.CortarCabelo(*Cliente)
+		wg.Done()
+	}
+}
+
+func (b *Barbearia) CortarCabelo(cl Cliente) {
+	b.ocupadas--
+	fmt.Println("Cliente", cl.id, "esta cortando o cabelo")
+	time.Sleep(1000 * time.Millisecond)
+	fmt.Println("Cliente", cl.id, "acabou de cortar o cabelo")
+}
+
+func (b *Barbearia) AbrirBarbearia(wg *sync.WaitGroup) {
+	for Cliente := range b.entrada {
+		if b.ocupadas < b.cadeiras {
+			b.ocupadas++
+			go Cliente.Sentar(b.cortando)
+		} else {
+			fmt.Println("Cliente", Cliente.id, "tentou entrar mas estava lotado")
+			wg.Done()
+		}
+	}
+
+}
+
+type Cliente struct {
+	id int
+}
+
+func (c *Cliente) Entrar(entrada chan *Cliente) {
+	fmt.Println("Cliente", c.id, "tentou entrar")
+	entrada <- c
+}
+
+func (c *Cliente) Sentar(cortar chan *Cliente) {
+	fmt.Println("Cliente", c.id, "sentou na espera")
+	c.DesejoCortarCabelo(cortar)
+}
+
+func (c *Cliente) DesejoCortarCabelo(cortar chan *Cliente) {
+	cortar <- c
+}
