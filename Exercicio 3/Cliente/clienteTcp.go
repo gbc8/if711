4ee1135ec561f70d6d4f97aa7ab4d36944857365
MathevsBatista <matheusisidoro09@gmail.com>
2022-08-20 23:42:28 -0300

    Mudando para Lorem ipsum

--- Exercicio 3/Cliente/clienteTcp.go
+++ Exercicio 3/Cliente/clienteTcp.go
@@ -15,7 +15,7 @@ func IniciarClienteTCP() {
 	}
 	comeco := time.Now()
 	for i := 0; i < 10000; i++ {
-		texto := "Nelson Rosa"
+		texto := "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 		c.Write([]byte(texto + "\n"))
 
 		_, _ = bufio.NewReader(c).ReadString('\n')
--- Exercicio 3/Cliente/clienteUdp.go
+++ Exercicio 3/Cliente/clienteUdp.go
@@ -20,7 +20,7 @@ func IniciarClienteUDP() {
 
 	comeco := time.Now()
 	for i := 0; i < 10000; i++ {
-		data := []byte("Nelson Rosa" + "\n")
+		data := []byte("Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." + "\n")
 		_, err = c.Write(data)
 		if err != nil {
 			fmt.Println(err)
